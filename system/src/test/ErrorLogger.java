dc0588e36dd3e619b69636983591c219ca6a331f
Jannik Davidsen <jannik@cms-net.dk>
2016-11-23 12:42:54 +0100

    Adding system

---
+++ system/src/test/ErrorLogger.java
@@ -0,0 +1,10 @@
+package test;
+
+public class ErrorLogger {
+
+	public static void main(String[] args) {
+		
+	}
+
+	
+}
---
+++ system/src/test/HashingModifier.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.util.HashMap;
+
+public class HashingModifier {
+
+	public static void main(String[] args) {
+		HashMap<String, String> hm = new HashMap<String, String>();
+		String pw = "password";
+		String user = "test@localhost.com";
+		String anotherUser = "test@remotehost.com";
+		hm.put(user, pw);
+		String result = hm.get(user);
+		try {
+			if (!result.equals(null))
+				System.out.println(result);
+		} catch (NullPointerException ex) {
+			System.err.println("User not found");
+		}
+	}
+}
