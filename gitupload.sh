e1ba2fb2bd31d691bca3cd577157c4626da24dc7
jannik <shyeinua@gmail.com>
2016-12-19 11:12:45 +0100

    Removed linux scripts

--- gitupload.sh
+++
@@ -1,7 +0,0 @@
-git add -A --all
-echo "Indtast commit besked"
-read message
-git commit -m $message
-git push origin master
-pause
-
--- linux upload.sh
+++
@@ -1,7 +0,0 @@
-git add -A --all
-echo Indtast commit besked
-set /p message=d
-git commit -m "%message%"
-git push origin master
-echo %message%
-pause
